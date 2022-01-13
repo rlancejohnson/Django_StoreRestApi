9160ee6a3cd866673f7650a67dca1c4f2a5544af
Lance Johnson <rlancejohnson@gmail.com>
2022-01-13 11:13:19 -0700

    Layer in limit and offset pagination and improve filter logic

--- Exercise Files/01_04_begin/demo/store/api_views.py
+++ Exercise Files/01_04_begin/demo/store/api_views.py
@@ -1,33 +1,43 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from store.serializers import ProductSerializer
 from store.models import Product
 
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
+
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = (DjangoFilterBackend, )
+
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', )
+    search_fields = ('name', 'description')
+
+    pagination_class = ProductsPagination
+
+    def getQueryset(self):
+        return self.queryset
 
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
 
-        queryset = Product.objects.all()
+        queryset = self.getQueryset()
 
         from django.utils import timezone
         now = timezone.now()
 
-        if on_sale is None:
-            return super().get_queryset()
-
-        elif on_sale.lower() == 'true':
+        if on_sale is not None and on_sale.lower() == 'true':
             return queryset.filter(
                 sale_start__lte = now,
                 sale_end__gte = now,
             )
 
-        elif on_sale.lower() == 'false':
+        elif on_sale is not None and on_sale.lower() == 'false':
             return queryset.filter(
                 sale_start__lt = now,
                 sale_end__lt = now,
