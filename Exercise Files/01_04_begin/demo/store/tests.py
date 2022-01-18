a6a649c0417f18a52f0b289dd8aaebfe9b479cde
Lance Johnson <rlancejohnson@gmail.com>
2022-01-18 14:14:10 -0700

    Add product photo upload test

--- Exercise Files/01_04_begin/demo/store/tests.py
+++ Exercise Files/01_04_begin/demo/store/tests.py
@@ -1,4 +1,8 @@
+import os.path
+from django.conf import settings
+
 from rest_framework.test import APITestCase
+
 from store.models import Product
 
 class ProductCreateTestCase(APITestCase):
@@ -69,3 +73,33 @@ class ProductUpdateTestCase(APITestCase):
         updated = Product.objects.get(id=product.id)
 
         self.assertEqual(updated.name, 'New Product')
+
+    def test_upload_product_photo(self):
+        product = Product.objects.first()
+        original_photo = product.photo
+        photo_path = os.path.join(
+            settings.MEDIA_ROOT,
+            'products',
+            'vitamin-iron.jpg'
+        )
+        with open(photo_path, 'rb') as photo_data:
+            response = self.client.patch(f'/api/v1/products/{product.id}/', {
+                'photo': photo_data
+            }, format='multipart')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertNotEqual(response.data['photo'], original_photo)
+
+        try:
+            updated = Product.objects.get(id=product.id)
+            expected_photo = os.path.join(
+                settings.MEDIA_ROOT,
+                'products',
+                'vitamin-iron',
+            )
+
+            self.assertTrue(updated.photo.path.startswith(expected_photo))
+
+        finally:
+            os.remove(updated.photo.path)
